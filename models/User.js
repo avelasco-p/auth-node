99ee5cf063ee7500fcf5afc348cb1069e1b5591f
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-11-20 12:21:35 -0600

    added User schema for mongodb added new libraries dependencies

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+		trim: true,
+	},
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
