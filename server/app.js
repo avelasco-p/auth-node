0fe259a993604a165d262957dc1fea158084aa59
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-11-21 11:56:31 -0600

    added CORS handling

--- server/app.js
+++ server/app.js
@@ -12,13 +12,30 @@ const loginRoutes = require('./api/routes/login');
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+
+//adding headers to response
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST'); 
+        return res.status(200).json({});
+    }
+})
+
+
+//routes to handle
 app.use('/login', loginRoutes);
 
 
+//root route
 app.get('/', (req, res, next) => {
     res.sendFile('./client/index.html', {root: __dirname + '/../'});
 });
 
+//not found page
 app.use('*', (error, req, res, next) => {
     res.status(404);
 });
